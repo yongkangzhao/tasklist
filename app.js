95832943cbb5518047ba9cc868aa5eada0d7911f
Yongkang Zhao <cookieyong@gmail.com>
2020-11-05 21:48:15 -0800

    implemented add task feature

---
+++ app.js
@@ -0,0 +1,44 @@
+// Define UI vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// load all event listeners
+loadEventListeners();
+
+function loadEventListeners(){
+    form.addEventListener('submit', addTask);
+}
+
+
+function addTask(e){
+    if(taskInput.value ===''){
+        alert('Add a task');
+    }
+
+    // create li element
+    const li = document.createElement('li');
+    // add class
+    li.className = 'collection-item';
+    // create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // create new link element
+    const link = document.createElement('a');
+    // add class
+    link.className = 'delete-item secondary-content';
+    // add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // append the link to li
+    li.appendChild(link);
+    // append li to ul
+    taskList.appendChild(li);
+
+    taskInput.value='';
+    console.log(li);
+
+
+
+    e.preventDefault();
+}
